8235d75e591485d99be4830db14eb6372c625673
keepwonder <qiangjiong1993@163.com>
2017-10-30 23:57:36 +0800

    add manager.py

---
+++ manager.py
@@ -0,0 +1,20 @@
+# coding:utf-8
+from flask_script import Manager,Server
+from app import app
+
+manager = Manager(app)
+manager.add_command('server', Server())
+
+@manager.shell
+def make_shell_context():
+	"""Create a python CLI.
+
+	return: Default import object
+	type: `Dcit`
+	"""
+	# 确保有导入Flask app obejct,否则启动的CLI上下文中仍然没有app对象
+	return dict(app=app)
+
+if __name__ == '__main__':
+	manager.run()
+	
